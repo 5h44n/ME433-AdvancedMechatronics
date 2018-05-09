3d6997988f4145bf221995a8793ef13e63d1c5e3
shaan savarirayan <shaan@u.northwestern.edu>
2018-05-08 19:20:57 -0500

    HW5 code v1 finished
    inital completion of hw 5 code

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -27,7 +27,7 @@
 #pragma config FPLLIDIV = DIV_2     //  divide input clock to be in range 4-5MHz
 #pragma config FPLLMUL = MUL_24     //  multiply clock after FPLLIDIV
 #pragma config FPLLODIV = DIV_2     //  divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = DIV_12    //  divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLIDIV = DIV_2    //  divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
 #pragma config UPLLEN = ON          //  USB clock on
 
 
