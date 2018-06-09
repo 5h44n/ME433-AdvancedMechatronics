832f6df2c7fbf1b52e993c3b234deaae697f802f
shaan savarirayan <shaan@u.northwestern.edu>
2018-06-08 22:29:54 -0500

    HW9 done
    CDC working with IMU + video

---
+++ HW9/firmware/HW9.X/build/default/production/LSM6DS33.o.d
@@ -0,0 +1,3 @@
+build/default/production/LSM6DS33.o:  \
+LSM6DS33.c  \
+LSM6DS33.h 
---
+++ HW9/firmware/HW9.X/i2c.h
@@ -0,0 +1,21 @@
+// Header file for i2c.c
+// helps implement use I2C2 as a master without using interrupts
+
+#ifndef I2C_H
+#define	I2C_H
+
+#define WRITE 0                     // write bit
+#define READ 1                      // read bit
+
+void i2c_master_setup(void);              // set up I2C 1 as a master, at 100 kHz
+
+void i2c_master_start(void);              // send a START signal
+void i2c_master_restart(void);            // send a RESTART signal
+void i2c_master_send(unsigned char byte); // send a byte (either an address or data)
+unsigned char i2c_master_recv(void);      // receive a byte of data
+void i2c_master_ack(int val);             // send an ACK (0) or NACK (1)
+void i2c_master_stop(void);               // send a stop
+void i2c_read_multiple(unsigned char address, unsigned char reg, unsigned char *data, int length);
+
+#endif	/* I2C_H */
+
